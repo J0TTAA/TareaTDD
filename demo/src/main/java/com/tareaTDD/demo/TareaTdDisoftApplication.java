d8ae37926da846f183d1db6e0c1ce080e21d9d3f
Joaquin Antonio Escanilla Arauco <j.escanilla01@ufromail.cl>
2024-06-04 14:07:57 -0400

    setup

---
+++ demo/src/main/java/com/tareaTDD/demo/TareaTdDisoftApplication.java
@@ -0,0 +1,13 @@
+package com.tareaTDD.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaTdDisoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaTdDisoftApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/tareaTDD/demo/TareaTdDisoftApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tareaTDD.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaTdDisoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+	
+
+
+}
